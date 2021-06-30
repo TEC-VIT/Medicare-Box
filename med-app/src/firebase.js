cd8390ba56c2efd16e2b874664e1d0caa787dd86
Ankita Mandal <66859175+Ankita-Mandal@users.noreply.github.com>
2021-06-30 22:03:43 +0530

    firebase added

---
+++ med-app/src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCdoiR98PiYs4D-85cNktoGkqyyxEABEBc",
+    authDomain: "medicare-box.firebaseapp.com",
+    projectId: "medicare-box",
+    storageBucket: "medicare-box.appspot.com",
+    messagingSenderId: "866158574923",
+    appId: "1:866158574923:web:c1cd16e18960c045d3fcce",
+    measurementId: "G-9B9B131KG8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
