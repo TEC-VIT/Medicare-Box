b0303ec77a580b381c2517b9e5ccb60f62bdb316
yashrajgure <yashrajgure0808@gmail.com>
2021-07-02 18:16:36 +0530

    add button created

--- med-app/src/App.js
+++ med-app/src/App.js
@@ -1,69 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 import firebase from './firebase.js';
+import Home from './MainPage/home.js';
 
-class App extends Component {
+// class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      currentItem: '',
-      username: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+//   constructor() {
+//     super();
+//     this.state = {
+//       currentItem: '',
+//       username: ''
+//     }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-    console.log('change');
-  }
+//   handleChange(e) {
+//     this.setState({
+//       [e.target.name]: e.target.value
+//     });
+//     console.log('change');
+//   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-      title: this.state.currentItem,
-      user: this.state.username
-    }
-    console.log('submit pre push');
-    itemsRef.push(item);
-    console.log('submit post push');
-    this.setState({
-      currentItem: '',
-      username: ''
-    });
-    console.log('submit done');
-  }
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     const itemsRef = firebase.database().ref('items');
+//     const item = {
+//       title: this.state.currentItem,
+//       user: this.state.username
+//     }
+//     console.log('submit pre push');
+//     itemsRef.push(item);
+//     console.log('submit post push');
+//     this.setState({
+//       currentItem: '',
+//       username: ''
+//     });
+//     console.log('submit done');
+//   }
 
-  render() {
-    return (
-      <div className='app'>
-        <header>
-            <div className='wrapper'>
-              <h1>Fun Food Friends</h1>
+//   render() {
+//     return (
+//       <div className='app'>
+//         <header>
+//             <div className='wrapper'>
+//               <h1>Fun Food Friends</h1>
               
-            </div>
-        </header>
-        <div className='container'>
-          <section className="add-item">
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-              <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-              <button>Add Item</button>
-            </form>
-          </section>
-          <section className='display-item'>
-            <div className='wrapper'>
-              <ul>
-              </ul>
-            </div>
-          </section>
-        </div>
-      </div>
-    );
-  }
+//             </div>
+//         </header>
+//         <div className='container'>
+//           <section className="add-item">
+//             <form onSubmit={this.handleSubmit}>
+//               <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+//               <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+//               <button>Add Item</button>
+//             </form>
+//           </section>
+//           <section className='display-item'>
+//             <div className='wrapper'>
+//               <ul>
+//               </ul>
+//             </div>
+//           </section>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const App= () =>{
+  return(
+    <Home/>
+  )
 }
 export default App;
--- med-app/src/MainPage/home.js
+++ med-app/src/MainPage/home.js
@@ -1,3 +1,15 @@
-const Home=() =>{
+import './home.css'
+import {BiPlusMedical} from "react-icons/bi";
 
+const Home=() =>{
+    return(
+        <div className="home">
+            Home Page
+            <div className="add-med">
+                <BiPlusMedical className="add-icon" />
+            </div>
+        </div>
+    )
 }
+
+export default Home
