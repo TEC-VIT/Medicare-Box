595b5b14bbb884529fa576d123773f99be2f96c5
yashrajgure <yashrajgure0808@gmail.com>
2021-07-08 19:53:04 +0530

    home and fonts

--- med-app/src/MainPage/home.js
+++ med-app/src/MainPage/home.js
@@ -1,16 +1,22 @@
 import './home.css'
 import {GoPlus} from "react-icons/go";
 import { Link } from 'react-router-dom';
+import { MdAccountCircle } from "react-icons/md";
 
 const Home=() =>{
     return(
         <div className="home">
-            Home Page
             <Link to='/newname'>
                 <div className="add-med" onClick=''>
                     <GoPlus className="add-icon" />
                 </div>
             </Link>
+            <div className='top-row'>
+                <MdAccountCircle className="acc-icon" />
+                <div className='heading'>
+                    Hey Yash!
+                </div>
+            </div>
         </div>
     )
 }
