0027bffa56db25ca2c6e7f8ef032618aec283dbe
yashrajgure <yashrajgure0808@gmail.com>
2021-07-09 21:30:03 +0530

    home page 1

--- med-app/src/MainPage/home.js
+++ med-app/src/MainPage/home.js
@@ -3,6 +3,8 @@ import {GoPlus} from "react-icons/go";
 import { Link } from 'react-router-dom';
 import { MdAccountCircle } from "react-icons/md";
 
+const data=[]
+
 const Home=() =>{
     return(
         <div className="home">
@@ -15,6 +17,21 @@ const Home=() =>{
                 <MdAccountCircle className="acc-icon" />
                 <div className='heading'>
                     Hey Yash!
+                </div>
+            </div>
+            <div className='card'>
+                <div className='comp-name'>Compartment 1</div>
+                <div className='content'>
+                    <div className='details'>
+                        <div className='med-name'>Medicine: <b>Remdesivir</b></div>
+                        <div className='repeat'><i>Repeats daily, twice a day</i></div>
+                        <div className='remain'>Pills left: <b>15</b></div>
+                        <div className='refill'>Refill before: <b>10th July, 2021</b></div>
+                    </div>
+                    <div className='options'>
+                        <div className='edit'>Edit</div>
+                        <div className='refill-button'>Refill</div>
+                    </div>
                 </div>
             </div>
         </div>
